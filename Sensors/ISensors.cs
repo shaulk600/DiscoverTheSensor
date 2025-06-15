f29769f1ff5faa6459528be73bb73898400e83ea
Shaul Kimel <shaullk600@gmail.com>
2025-06-15 12:45:05 +0300

    Adding a base class: Adding Sensors to the program including an Interface to implement the main methods

---
+++ Sensors/ISensors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DiscoverTheSensor.Agent;
+namespace DiscoverTheSensor.Sensors
+{
+    internal interface ISensors
+    {
+        void Activate();
+        bool IsItWithBreakLimit(); //האם הוא עם הגבלת שבירות
+
+        void InsertRandomSensorsIntoAnObject(); // להביא סנסורים אל האובייקטים של agent
+
+
+    }
+}
