e6dc32e8c83554e53557e4af95bbc43fac889a0c
Shaul Kimel <shaullk600@gmail.com>
2025-06-15 16:56:00 +0300

    Adding a specific object constructor definition for the class and implementing a random function on a Sensor object in the class. Adding an advanced interface - to be implemented at a later stage in the project. In addition, improving the current interface and building the standard sensors class.

---
+++ Sensors/ISensorsAdvancing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscoverTheSensor.Sensors
+{
+    internal interface ISensorsAdvancing
+    {
+        int NumberOfSessions { get; set; } // כמות ההפעלות
+        void AttackAndTakedownInOne(); // במידה ויש לנו את  מספר האובייקטים שאנחנו צריכים להוריד - אז לרדת ב1 כל פעם - אולי לחלק את זה לשתי InterFace
+    }
+}
