0fa6f1250efb840d4bdb4cb464c58273fadb0b22
Shaul Kimel <shaullk600@gmail.com>
2025-06-17 17:17:42 +0300

    Update progeam.cs

--- Program.cs
+++ Program.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+
+using DiscoverTheSensor.Menu;
+using DiscoverTheSensor.Test;
 
 namespace DiscoverTheSensor
 {
@@ -10,6 +14,8 @@ namespace DiscoverTheSensor
     {
         static void Main(string[] args)
         {
+            //T.AgentSimple();
+            MenuRegular a = new MenuRegular();
         }
     }
 }
